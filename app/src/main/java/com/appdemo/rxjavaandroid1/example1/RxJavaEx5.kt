2a6abee894be95d15e8de30764f0dc136ee2c38f
darshanrahate <rahate.darshan@gmail.com>
2023-08-13 15:06:02 +0530

    Just Operator using fromArray and range operator.

--- app/src/main/java/com/appdemo/rxjavaandroid1/example1/RxJavaEx5.kt
+++ app/src/main/java/com/appdemo/rxjavaandroid1/example1/RxJavaEx5.kt
@@ -7,23 +7,56 @@ import io.reactivex.rxjava3.observers.DisposableObserver
 
 class RxJavaEx5(private val scheduler: SchedulerProvider) {
     lateinit var observable: Observable<String>
+    lateinit var observableFromArray: Observable<String>
+    lateinit var observableRanges: Observable<Int>
     private val compositeDisposable = CompositeDisposable()
 
     fun callRxFunctionality() {
         observable = Observable.just("Hello 1", "Hello 2", "Hello 3")
-
         compositeDisposable.add(
             observable
                 .subscribeOn(scheduler.ui())
                 .observeOn(scheduler.io())
                 .subscribeWith(getObserver())
         )
+
+        observableFromArray = Observable.fromArray("Hello 1", "Hello 2", "Hello 3")
+        compositeDisposable.add(
+            observableFromArray
+                .subscribeOn(scheduler.ui())
+                .observeOn(scheduler.io())
+                .subscribeWith(getObserver())
+        )
+
+        observableRanges = Observable.range(1, 10)
+        compositeDisposable.add(
+            observableRanges
+                .subscribeOn(scheduler.ui())
+                .observeOn(scheduler.io())
+                .subscribeWith(getObserverInt())
+        )
     }
 
     private fun getObserver(): DisposableObserver<String> {
         return object : DisposableObserver<String>() {
             override fun onNext(t: String) {
-                println(":::== onNext : " + t)
+                println(":::== String : onNext : " + t)
+            }
+
+            override fun onError(e: Throwable) {
+                println(":::== onError : ")
+            }
+
+            override fun onComplete() {
+                println(":::== onComplete : ")
+            }
+        }
+    }
+
+    private fun getObserverInt(): DisposableObserver<Int> {
+        return object : DisposableObserver<Int>() {
+            override fun onNext(t: Int) {
+                println(":::== Int : onNext : " + t)
             }
 
             override fun onError(e: Throwable) {
--- app/src/test/java/com/appdemo/rxjavaandroid1/example1/RxJavaEx5Test.kt
+++ app/src/test/java/com/appdemo/rxjavaandroid1/example1/RxJavaEx5Test.kt
@@ -7,8 +7,8 @@ import org.junit.Test
 class RxJavaEx5Test {
 
     @Test
-    fun test1() {
-       val rxJavaEx5 = RxJavaEx5(TestSchedulerProvider(TestScheduler()))
+    fun `test for just`() {
+        val rxJavaEx5 = RxJavaEx5(TestSchedulerProvider(TestScheduler()))
         rxJavaEx5.callRxFunctionality()
         val observable = rxJavaEx5.observable.test()
 
@@ -16,6 +16,41 @@ class RxJavaEx5Test {
         observable.assertValueAt(0, "Hello 1")
         observable.assertValueAt(1, "Hello 2")
         observable.assertValueAt(2, "Hello 3")
+        observable.assertComplete()
+    }
+
+    @Test
+    fun `test for fromArray`() {
+        val rxJavaEx5 = RxJavaEx5(TestSchedulerProvider(TestScheduler()))
+        rxJavaEx5.callRxFunctionality()
+        val observable = rxJavaEx5.observableFromArray.test()
+
+        observable.assertValues("Hello 1", "Hello 2", "Hello 3")
+        observable.assertValueAt(0, "Hello 1")
+        observable.assertValueAt(1, "Hello 2")
+        observable.assertValueAt(2, "Hello 3")
+        observable.assertComplete()
+    }
+
+    @Test
+    fun `test for ranges`() {
+        val rxJavaEx5 = RxJavaEx5(TestSchedulerProvider(TestScheduler()))
+        rxJavaEx5.callRxFunctionality()
+        val observable = rxJavaEx5.observableRanges.test()
+
+        observable.assertValues(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+        observable.assertValueAt(0, 1)
+        observable.assertValueAt(1, 2)
+        observable.assertValueAt(2, 3)
+        observable.assertValueAt(3, 4)
+        observable.assertValueAt(4, 5)
+        observable.assertValueAt(5, 6)
+        observable.assertValueAt(6, 7)
+        observable.assertValueAt(7, 8)
+        observable.assertValueAt(8, 9)
+        observable.assertValueAt(9, 10)
+
         observable.assertComplete()
     }
 }
